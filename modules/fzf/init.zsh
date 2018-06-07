0cca809cf6dfc20b6df297cc06c91b4b4795b071
J. W. Smith <jwsmith2spam at gmail dot com>
2018-06-07 18:12:43 -0400

    Added a small fzf shim plugin
    The plugin literally just sources ~/.fzf.zsh, if it exists, and
    otherwise it does nothing.  Why work any harder?  :P
    
    Next, I'll probably have it try to install fzf if it doesn't already
    exist, down the road.  That would be handy.

---
+++ modules/fzf/init.zsh
@@ -0,0 +1,11 @@
+# fzf is a fuzzy-search engine for shells, available at
+# https://github.com/junegunn/fzf
+
+# This plugin is tiny and just uses the default fzf config stuff.
+# So follow the given instructions in the fzf readme, but when you
+# run its installer script, run it with --no-update-rc:
+#   ~/.fzf/install --no-update-rc
+# to install the binaries and fzf config files, but leave config
+# files alone.
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
