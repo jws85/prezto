8d5e76b1759ce21ff7e37761368eedff6ec44cf4
J. W. Smith <jwsmith2spam at gmail dot com>
2018-06-25 19:15:44 -0400

    Make tmuxa attach by default to something
    I usually only ever use one tmux/screen session per box... so it is a
    little grating to have to constantly do
    
       tmuxa -t session
    
    Here, I just default a session name, so all I have to type is 'tmuxa'
    and it attaches, or 'tmuxa othersession' to get the other session.

--- modules/tmux/init.zsh
+++ modules/tmux/init.zsh
@@ -27,6 +27,8 @@ if zstyle -t ':prezto:module:tmux' colors-256; then
   _tmux_256_colors="-2"
 fi
 
+zstyle -s ':prezto:module:tmux:session' name tmux_session || tmux_session='prezto'
+
 if [[ -z "$TMUX" && -z "$EMACS" && -z "$VIM" && -z "$INSIDE_EMACS" && -z "$VSCODE_PID" ]] && ( \
   ( [[ -n "$SSH_TTY" ]] && zstyle -t ':prezto:module:tmux:auto-start' remote ) ||
   ( [[ -z "$SSH_TTY" ]] && zstyle -t ':prezto:module:tmux:auto-start' local ) \
@@ -35,7 +37,6 @@ if [[ -z "$TMUX" && -z "$EMACS" && -z "$VIM" && -z "$INSIDE_EMACS" && -z "$VSCOD
 
   # Create a 'prezto' session if no session has been defined in tmux.conf.
   if ! tmux has-session 2> /dev/null; then
-    zstyle -s ':prezto:module:tmux:session' name tmux_session || tmux_session='prezto'
     tmux \
       new-session -d -s "$tmux_session" \; \
       set-option -t "$tmux_session" destroy-unattached off &> /dev/null
@@ -49,5 +50,11 @@ fi
 # Aliases
 #
 
-alias tmuxa="tmux $_tmux_256_colors $_tmux_iterm_integration new-session -A"
+function tmuxa {
+  session="$tmux_session"
+  if [ -n "$1" ]; then
+    session="$1"
+  fi
+  exec tmux $_tmux_256_colors $_tmux_iterm_integration new-session -A -t $session
+}
 alias tmuxl='tmux list-sessions'
